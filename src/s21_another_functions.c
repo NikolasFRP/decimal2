927be1210548b4ec24c87b998329f4cfa6f8a43b
Severobata <pelageyagerasimova@MacBook-Air-Pelageya.local>
2024-01-18 19:55:51 +0300

    comments

--- src/s21_another_functions.c
+++ src/s21_another_functions.c
@@ -2,26 +2,47 @@
 // Округляет указанное Decimal число до ближайшего целого числа в сторону
 // отрицательной
 int s21_floor(s21_decimal value, s21_decimal *result) {
+  // выставляет все нули
   s21_set_null_decimal(result);
-  s21_set_null_decimal(result);
+  // округление по нижней границе
   s21_truncate(value, result);
-  if (s21_get_sign(value) && !s21_is_equal(*result, value)) {
-    s21_sub(*result, ((s21_decimal){{1, 0, 0,
-                                     0}}), result);
+  // Это условие проверяет знак числа value и сравнивает усеченное значение
+  if (s21_get_sign(value) && !s21_is_equal(*result, value))
+  //  Если исходное число отрицательное и после усечения результат не совпадает
+  //  с исходным числом, к результату прибавляется -1. Функция всегда возвращает
+  //  0.
+  {
+    s21_sub(*result, ((s21_decimal){{1, 0, 0, 0}}), result);
   }
   return 0;
 }
 // Округляет Decimal до ближайшего целого числа.
 int s21_round(s21_decimal value, s21_decimal *result) {
+  // устанавливает значение result в ноль
   s21_set_null_decimal(result);
+  // объявление переменных для хранения временных значений
   long double fl_to_round = 0.0;
   long double difference = 0;
+  // конвертация значения value в тип long double и сохранение в переменной
+  // fl_to_round
   s21_from_decimal_to_double(value, &fl_to_round);
+  // округление
   fl_to_round = round(fl_to_round);
+  // если число отрицательно
   if (fl_to_round < 0.0) {
+    // устанавливает 127 бит в еденицу (чтобы было положительным)
     s21_set_bit_on_index(result, 127, 1);
+    // умножает все число на -1 делая его положительным
     fl_to_round *= -1;
   }
+  // округляется вниз и затем делится на 2. Это выполняется для получения
+  // следующего бита числа. Округление вниз гарантирует, что мы получаем
+  // целочисленное значение при делении на 2. Разница между fl_to_round и его
+  // округленным значением используется для определения, является ли следующий
+  // бит равным 1. Если difference больше 0, это означает, что есть дробная
+  // часть, и следующий бит должен быть установлен в 1.
+  // происходит установка соответствующего бита в результате. Если difference
+  // больше 0, то бит устанавливается в 1, иначе он устанавливается в 0.
   for (int i = 0; fl_to_round >= 1 && i < START_INFO; i++) {
     fl_to_round = floor(fl_to_round) / 2;
     difference = fl_to_round - floor(fl_to_round);
@@ -37,17 +58,35 @@ int s21_round(s21_decimal value, s21_decimal *result) {
 // Возвращает целые цифры указанного Decimal числа; любые дробные цифры
 // отбрасываются, включая конечные нули.
 int s21_truncate(s21_decimal value, s21_decimal *result) {
+  // устанавливает значение result в ноль
   s21_set_null_decimal(result);
+  // ставим дефолт в ноль
   float difference = 0;
+  // если масштаб числа value не равен нулю
   if (s21_get_scale(value)) {
+    // конвертация значения value в тип long double и сохранение в переменной
+    // fl_to_int
     long double fl_to_int = 0.0;
     s21_from_decimal_to_double(value, &fl_to_int);
+    // округление вниз
     fl_to_int = trunc(fl_to_int);
+    // если усеченное значение отрицательное, устанавливается соответствующий
+    // бит в result
+    // и fl_to_int делается положительным
     if (fl_to_int < 0.0) {
       s21_set_bit_on_index(result, 127, 1);
 
       fl_to_int *= -1;
     }
+    // округляется вниз и затем делится на 2. Это выполняется для получения
+    // следующего бита числа. Округление вниз гарантирует, что мы получаем
+    // целочисленное значение при делении на 2.
+    // Разница между fl_to_round и его округленным значением используется для
+    // определения, является ли следующий бит равным 1. Если difference больше
+    // 0, это означает, что есть дробная часть, и следующий бит должен быть
+    // установлен в 1.
+    // происходит установка соответствующего бита в результате. Если difference
+    // больше 0, то бит устанавливается в 1, иначе он устанавливается в 0.
     for (int i = 0; fl_to_int >= 1 && i < START_INFO; i++) {
       fl_to_int = floor(fl_to_int) / 2;
       difference = fl_to_int - floor(fl_to_int);
@@ -59,17 +98,22 @@ int s21_truncate(s21_decimal value, s21_decimal *result) {
     }
     s21_set_scale(result, 0);
   } else {
+    // копирование битов value в result
     for (int i = 0; i < 4; ++i) {
       result->bits[i] = value.bits[i];
     }
   }
   return 0;
 }
-// Возвращает результат умножения указанного Decimal на -1.
+// Возвращает результат умножения указанного Decimal на -1. (смена знака)
 int s21_negate(s21_decimal dec, s21_decimal *result) {
+  // задаем все нули
   s21_set_null_decimal(result);
+  // получение знака числа
   int sign = s21_get_sign(dec);
+  // копирование
   *result = dec;
+  // 0 - отрицательно, 1 - положительное
   if (sign == 1) {
     s21_set_bit_on_index(result, 127, 0);
 
--- src/s21_comparison.c
+++ src/s21_comparison.c
@@ -1,5 +1,6 @@
 #include "s21_decimal.h"
 
+// позволяет циклически переключать значение переменной между 1 и 2.
 void s21_comparison_reverse(int *flag) {
   if (*flag == 1) {
     *flag = 2;
@@ -8,13 +9,23 @@ void s21_comparison_reverse(int *flag) {
   }
 }
 
+// 0 - числа равны побитово
+// 1 - первое больше побитово
+// 2 - второе больше побитово
 int s21_comparison_bits(s21_decimal d1, s21_decimal d2) {
+  // хранение значений чисел в битах
   int bit1 = 0;
   int bit2 = 0;
+  // flag - для указания результата сравнения.
   int flag = 0;
+  // Цикл проходит по битам чисел начиная с 95-го (и до 0) и прекращается, если
+  // флаг становится ненулевым.
   for (int i = 95; i >= 0 && !flag; --i) {
+    // Получение битов чисел d1 и d2 на текущем индексе i.
     bit1 = s21_get_bit_on_index(d1, i);
     bit2 = s21_get_bit_on_index(d2, i);
+    // Сравнение битов. Если бит1 больше бит2, устанавливается флаг в 1.
+    // Если бит1 меньше бит2, устанавливается флаг в 2.
     if (bit1 > bit2) {
       flag = 1;
     } else if (bit1 < bit2) {
@@ -24,6 +35,10 @@ int s21_comparison_bits(s21_decimal d1, s21_decimal d2) {
   return flag;
 }
 
+// сравнение флагов
+// 0 - раны
+// 1 - первый больше
+// 2 - второй больше
 int s21_greater_num(int a, int b) {
   int flag = 0;
   if (a > b) {
@@ -36,23 +51,38 @@ int s21_greater_num(int a, int b) {
   return flag;
 }
 
+// Сравнение чисел с учетом знака и масштаба
 int s21_comparison(s21_decimal d1, s21_decimal d2) {
+  // для хранения результата сравнения.
   int flag = 0;
+  // вычесление и извлечение экспоненты/масштаба далее вычисление их раницы
   int scale_dif = (s21_get_scale(d1) - s21_get_scale(d2));
+  // Нормализация, выравнивание чисел по масштабу, чтобы их представление было
+  // корректным для последующего сравнения
   s21_normalize(&d1, &d2);
+  // Сравнение абсолютных значений чисел d1 и d2 и установка флага на основе
+  // результата.
   flag = s21_greater_num(!s21_get_sign(d1), !s21_get_sign(d2));
+  // если совпадают
   if (flag == 0) {
+    // если  разница в масштабах неотрицательна
     if (scale_dif >= 0) {
+      // измеряем и задаем флаг
       flag = s21_comparison_bits(d1, d2);
     } else {
+      // если разница в масштабах отрицательна, то делаем положительной
       scale_dif = -scale_dif;
       flag = s21_comparison_bits(d2, d1);
+      // задаем новый флаг сравнением побитово
       s21_comparison_reverse(&flag);
     }
+    // Инверсия результата, если оба числа отрицательны.
     if (s21_get_sign(d1) && s21_get_sign(d2)) {
       s21_comparison_reverse(&flag);
     }
   } else {
+    // Если флаг не равен 0 (различаются абсолютные значения), и оба числа равны
+    // нулю, устанавливается флаг в 0.
     if (d1.bits[0] == 0 && d1.bits[1] == 0 && d1.bits[2] == 0 &&
         d2.bits[0] == 0 && d2.bits[1] == 0 && d2.bits[2] == 0) {
       flag = 0;
@@ -60,27 +90,50 @@ int s21_comparison(s21_decimal d1, s21_decimal d2) {
   }
   return flag;
 }
-
+// сравнение двух чисел типа децимал
+// 1 - первый больше
+// 2 - второй больше
+// 0 - равны
 int s21_is_less(s21_decimal d1, s21_decimal d2) {
   return (s21_comparison(d1, d2) == 2) ? 1 : 0;
 }
 
+// проверка д1 меньше или равно д2
+// д1 меньше 2
+// д1 равно 0
+// д1 больше 1
 int s21_is_less_or_equal(s21_decimal d1, s21_decimal d2) {
   return (s21_comparison(d1, d2) == 2) || (s21_comparison(d1, d2) == 0) ? 1 : 0;
 }
 
+// д1 больше д2
+// д1 меньше 2
+// д1 равно 0
+// д1 больше 1
 int s21_is_greater(s21_decimal d1, s21_decimal d2) {
   return (s21_comparison(d1, d2) == 1) ? 1 : 0;
 }
 
+// д1 больше или равно д2
+// д1 меньше 2
+// д1 равно 0
+// д1 больше 1
 int s21_is_greater_or_equal(s21_decimal d1, s21_decimal d2) {
   return (s21_comparison(d1, d2) == 1) || (s21_comparison(d1, d2) == 0) ? 1 : 0;
 }
 
+// д1 равно д2
+// д1 меньше 2
+// д1 равно 0
+// д1 больше 1
 int s21_is_equal(s21_decimal d1, s21_decimal d2) {
   return (s21_comparison(d1, d2) == 0) ? 1 : 0;
 }
 
+// д1 не такой же как д2
+// д1 меньше 2
+// д1 равно 0
+// д1 больше 1
 int s21_is_not_equal(s21_decimal d1, s21_decimal d2) {
   return (s21_comparison(d1, d2) == 0) ? 0 : 1;
 }
